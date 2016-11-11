e4d1463bf1ccbbd14554cde0af5220695368472c
Luis Cajigas Martínez <luic87@gmail.com>
2016-11-11 13:04:28 +0100

    Finished

--- src/Maintenance.php
+++ src/Maintenance.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Luisinder\Middleware;
 
 use Psr\Http\Message\ServerRequestInterface;
@@ -7,17 +6,25 @@ use Psr\Http\Message\ResponseInterface;
 
 Class Maintenance
 {
-  protected $maintenanceMode;
-  protected $errorClass;
+	protected $maintenanceMode;
+	protected $errorObject;
 
-  public function __construct(bool $maintenanceMode = false, string $errorClass ="")
-  {
-    $this->$maintenanceMode = $maintenanceMode;
-    $this->errorClass = $errorClass;
-  }
+	public function __construct($maintenanceMode = false, $errorObject)
+	{
+		$this->maintenanceMode = $maintenanceMode;
+		$this->errorObject = $errorObject;
+	}
 
-public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
-{
-
-}
+	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, $next)
+	{
+		if($this->maintenanceMode)
+		{
+			$response
+			->withHeader('Content-type', 'application/json')
+			->getBody()
+			->write(json_encode($this->errorObject));
+			return $response;
+		}
+		return $response = $next($request, $response);
+	}
 }
